d04608d4c9fb71ea977dc3cf4ef602b542ba2a98
axel Thiel <taxelt@gmail.com>
2017-02-10 16:41:05 +0100

    Add user entity

---
+++ src/main/java/com/xamarinpocbanque/server/entity/user.java
@@ -0,0 +1,108 @@
+package com.xamarinpocbanque.server.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class user {
+
+	@Id
+    @SequenceGenerator(name = "mobithink_uid", sequenceName = "mobithink_uid", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mobithink_uid")
+    @Column(name="id")
+    private Long id;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "first_name")
+    private Long firstName;
+
+    
+    @Column(name = "login")
+    private Long login;
+    
+    @Column(name = "password")
+    private Long password;
+    
+    @Column(name = "email")
+    private String email ;
+    
+    @Column(name = "phone")
+    private int phone;
+    
+    @Column(name= "bank_id")
+    private int bankId;
+    
+    
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {					
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(Long firstName) {
+		this.firstName = firstName;
+	}
+
+	public Long getLogin() {
+		return login;
+	}
+
+	public void setLogin(Long login) {
+		this.login = login;
+	}
+
+	public Long getPassword() {
+		return password;
+	}
+
+	public void setPassword(Long password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public int getBankId() {
+		return bankId;
+	}
+
+	public void setBankId(int bankId) {
+		this.bankId = bankId;
+	}
+            
+    
+}
