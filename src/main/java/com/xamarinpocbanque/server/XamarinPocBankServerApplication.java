b93af2c388d3bd597bdefe64bbe484ee86db3ac4
axel Thiel <taxelt@gmail.com>
2017-02-10 14:25:28 +0100

    eclipse commit

---
+++ src/main/java/com/xamarinpocbanque/server/XamarinPocBankServerApplication.java
@@ -0,0 +1,13 @@
+package com.xamarinpocbanque.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XamarinPocBankServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XamarinPocBankServerApplication.class, args);
+		
+	}
+}
